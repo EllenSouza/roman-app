a50761207635403f8f684558f2313324316d8205
keviinsna <keevinsenna2001@gmail.com>
2023-05-31 16:27:26 -0300

    add developers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Form from "./components/form";
 function App() {
   return (
     <div className="App d-flex justify-content-center">
+      <div className="flex-column ">
+
       <div className="card mt-2 p-2 " style={{ width: "30rem" }}>
         <div className="card-body">
         <h5 className="card-title">Roman Conversor</h5>
@@ -14,6 +16,16 @@ function App() {
           </p>
         </div>
       </div>
+      <div className="mt-4">
+        <h7 className="my-3">
+              <span>Developed by: </span> 
+              <a href="https://github.com/EllenSouza">Ellen Almeida</a>
+              <span> and </span>
+              <a href="https://github.com/keviinsna">Kevin Sena</a>
+          </h7>
+      </div>
+      </div>
+
     </div>
   );
 }
