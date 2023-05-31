b1b29b052a362dd65e3a61533d672e4742a6771b
keviinsna <keevinsenna2001@gmail.com>
2023-05-31 18:34:05 -0300

    remove unnecessary state and fix button click

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ function App() {
   return (
     <div className="App d-flex justify-content-center">
       <div className="flex-column ">
-
-      <div className="card mt-2 p-2 " style={{ width: "30rem" }}>
-        <div className="card-body">
-        <h5 className="card-title">Roman Conversor</h5>
-          <Form />
-          <p className="mt-4">
-            Note: characters in ( ) are multiplied by 1000 and values ∈ [1,
-            3.999.999]
-          </p>
+        <div className="card mt-2 p-2 " style={{ width: "30rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Roman Conversor</h5>
+            <Form />
+            <p className="mt-4">
+              Note: characters in ( ) are multiplied by 1000 and values ∈ [1,
+              3.999.999]
+            </p>
+          </div>
+        </div>
+        <div className="mt-4">
+          <h7 className="my-3">
+            <span>Developed by: </span>
+            <a href="https://github.com/EllenSouza">Ellen Almeida</a>
+            <span> and </span>
+            <a href="https://github.com/keviinsna">Kevin Sena</a>
+          </h7>
         </div>
       </div>
-      <div className="mt-4">
-        <h7 className="my-3">
-              <span>Developed by: </span> 
-              <a href="https://github.com/EllenSouza">Ellen Almeida</a>
-              <span> and </span>
-              <a href="https://github.com/keviinsna">Kevin Sena</a>
-          </h7>
-      </div>
-      </div>
-
     </div>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -8,34 +8,25 @@ const initValues = {
 
 export default function Form() {
   const [input, setInput] = useState({ ...initValues });
-  const [inputResult, setInputResult] = useState("");
   const [isRoman2int, setIsRoman2int] = useState(false);
 
-  //change the conversion
-  function handleClick() {
-    console.log(isRoman2int, input);
-    setIsRoman2int(!isRoman2int);
-    setInput(initValues);
-    setInputResult("");
-  }
-
-  function handleChangeNumber(event) {
-    try {
-      if (isRoman2int) {
-        setInput((prev) => ({ ...prev, roman: event.target.value }));
-        if (validateRoman(event.target.value).length == 0)
-          setInputResult(roman2int(event.target.value));
-        else {
-          setInputResult(0);
-          alert(`Invalid Roman: ${event.target.value.toUpperCase()}`);
-        }
-      } else {
-        setInput((prev) => ({ ...prev, int: event.target.value }));
-        setInputResult(int2romanFast(event.target.value));
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  function handleChangeInput(event) {
+    if (isRoman2int) {
+      const inputResult =
+        validateRoman(event.target.value).length == 0
+          ? roman2int(event.target.value)
+          : 0;
+      setInput((prev) => ({
+        ...prev,
+        roman: event.target.value.toUpperCase(),
+        int: inputResult,
+      }));
+    } else
+      setInput((prev) => ({
+        ...prev,
+        int: event.target.value,
+        roman: int2romanFast(event.target.value),
+      }));
   }
 
   return (
@@ -44,19 +35,20 @@ export default function Form() {
         className="btn btn-secondary"
         type="button"
         style={{ width: "4rem" }}
+        onClick={() => setIsRoman2int(!isRoman2int)}
       >
-        <i className="bi bi-arrow-left-right" onClick={handleClick}></i>
+        <i className="bi bi-arrow-left-right"></i>
       </button>
-      <br />
       <div className="d-flex flex-row">
         <div className="">
           <label htmlFor="input1">{isRoman2int ? "Roman" : "Integer"}</label>
           <input
             id="input1"
             className="mt-2"
+            min={0}
             type={isRoman2int ? "text" : "number"}
             value={isRoman2int ? input.roman : input.int}
-            onChange={handleChangeNumber}
+            onChange={handleChangeInput}
             style={{ width: "8rem" }}
           />
         </div>
@@ -67,7 +59,7 @@ export default function Form() {
             id="input2"
             className="mt-2"
             disabled={true}
-            value={inputResult}
+            value={isRoman2int ? input.int : input.roman}
           />
         </div>
       </div>
