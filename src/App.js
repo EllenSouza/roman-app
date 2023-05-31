d9cbdb2abed7a5a168c24819a27f4d5d45967a8f
keviinsna <keevinsenna2001@gmail.com>
2023-05-31 16:11:09 -0300

    fix roman to int and change style

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import "./App.css";
 import Form from "./components/form";
 
 function App() {
-  
   return (
-    <div className="App">
-      <Form/>
+    <div className="App d-flex justify-content-center">
+      <div className="card mt-2 p-2 " style={{ width: "30rem" }}>
+        <div className="card-body">
+        <h5 className="card-title">Roman Conversor</h5>
+          <Form />
+          <p className="mt-4">
+            Note: characters in ( ) are multiplied by 1000 and values ∈ [1,
+            3.999.999]
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { int2romanFast, roman2int } from "../../src/roman";
+import { int2romanFast, roman2int, validateRoman } from "../../src/roman";
 
 const initValues = {
   int: 0,
@@ -23,7 +23,12 @@ export default function Form() {
     try {
       if (isRoman2int) {
         setInput((prev) => ({ ...prev, roman: event.target.value }));
-        setInputResult(roman2int(event.target.value));
+        if (validateRoman(event.target.value).length == 0)
+          setInputResult(roman2int(event.target.value));
+        else {
+          setInputResult(0);
+          alert(`Invalid Roman: ${event.target.value.toUpperCase()}`);
+        }
       } else {
         setInput((prev) => ({ ...prev, int: event.target.value }));
         setInputResult(int2romanFast(event.target.value));
@@ -35,13 +40,15 @@ export default function Form() {
 
   return (
     <>
-      <h5 className="mt-4">Roman Conversor</h5>
-
-      <button className="btn btn-secondary" type="button">
+      <button
+        className="btn btn-secondary"
+        type="button"
+        style={{ width: "4rem" }}
+      >
         <i className="bi bi-arrow-left-right" onClick={handleClick}></i>
       </button>
       <br />
-      <div className="row">
+      <div className="d-flex flex-row">
         <div className="">
           <label htmlFor="input1">{isRoman2int ? "Roman" : "Integer"}</label>
           <input
@@ -50,12 +57,18 @@ export default function Form() {
             type={isRoman2int ? "text" : "number"}
             value={isRoman2int ? input.roman : input.int}
             onChange={handleChangeNumber}
+            style={{ width: "8rem" }}
           />
         </div>
-        =
+        <div className="d-flex align-items-end"> = </div>
         <div>
           <label htmlFor="input2">{!isRoman2int ? "Roman" : "Integer"}</label>
-          <input id="input2" disabled={true} value={inputResult} />
+          <input
+            id="input2"
+            className="mt-2"
+            disabled={true}
+            value={inputResult}
+          />
         </div>
       </div>
     </>
